7c7b7e57cb9c44faff0284f064e9abfa2456156c
remotework1 <51304247+remotework1@users.noreply.github.com>
2019-09-22 23:19:50 -0400

    Add files via upload

---
+++ Lab4/hello.py
@@ -0,0 +1,15 @@
+# Lavonda Talbert
+# September 22, 2019
+# Author credit: (code by 'Automate the Boring Stuff with Python.')
+# THIS PROGRAM SAYS HELLO AND ASKS FOR MY NAME
+
+print ('HELLO WORLD!')
+print ('WHAT IS YOUR NAME?') # ASK FOR THEIR NAME
+myName = input ()
+print ('It is good to meet you, ' +myName)
+print ('The length of your name is:')
+print (len(myName))
+print ('What is your age?') # ask for their age
+myAge = input ()
+print ('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
